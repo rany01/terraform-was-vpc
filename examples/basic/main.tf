db4d3376f6299418b43e4ac72490eb2c68254a5b
rany <ranilson.reinaldo@gmail.com>
2025-04-16 14:31:57 -0300

    feat examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,4 +1,19 @@
+# Este módulo cria uma VPC na AWS com subnets públicas e privadas,
+# além de recursos como NAT Gateway e Internet Gateway.
 
+module "vpc" {
+  source = "github.com/rany01/terraform-aws-vpc?ref=v1.0.0"
+
+  vpc_name        = "giropops-vpc"
+  vpc_cidr        = "10.0.0.0/16"
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = ["us-east-1a", "us-east-1b"]
+  enable_nat_gateway = true
+}
+
+
+/*
 module "vpc" {
     source = "terraform-aws-modules/vpc/aws"
     
@@ -18,3 +33,4 @@ module "vpc" {
     }
   
 }
+*/
